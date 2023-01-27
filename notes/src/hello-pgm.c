2169b23d063aade60281231c901046d40eb652e4
morgan bergen <morgan@ku.edu>
2023-01-27 14:14:01 -0600

    Added hello-world program

--- notes/src/hello-pgm.c
+++ notes/src/hello-pgm.c
@@ -3,8 +3,12 @@
 //  DESCRIPTION     the standard I/O library provides a simple and efficient buffered stream I/O interface.  Input and output is mapped into logical data streams and the physical I/O characteristics are concealed.
 #include <stdio.h>
 int main() {
+
     printf("hello world\n");
+
 }
+
+
 
 /*
  * unix> gcc -o hello-pgm.exe hello-pgm.c
