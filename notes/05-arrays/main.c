2b8b5bb4833dedb385ec1c5866f62aa85f5c8ed3
morgan bergen <morgan@ku.edu>
2023-02-09 14:42:50 -0600

    removed extraneous 04-intro and test

---
+++ notes/05-arrays/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    
+int num[6] = { 2, 4, 12, 5, 45, 5 };
+
+}
+
+/*
+   int i, j, m;
+   int a[5] = {5, 1, 15, 20, 25};
+   i = ++a[1];
+   j = a[1]++;
+   m = a[i++];
+   printf("%d, %d, %d", i, j, m);
+*/
+
+/*
+   int a[4] = {1,2,3,4}; printf("%d", a[4]);
+*/
+
+/*
+ What will happen if in a C program you assign a value to an array element whose subscript exceeds the size of array?
+*/
